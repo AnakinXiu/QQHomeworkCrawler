64e063d402ac29650a8146ed9b210cf07e44d766
Ze.Xiu <nl919@ic.ac.uk>
2022-05-08 11:06:53 +0800

    Add homework view model

---
+++ QQHomeworkCrawler/HomeworkViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace QQHomeworkCrawler
+{
+    public class HomeworkViewModel
+    {
+        public string StudentName { get; set; }
+
+        public List<string> ImageUrl { get; set; }
+
+        public HomeworkViewModel(string studentName, params string[] imageUrls)
+        {
+            StudentName = studentName;
+
+            ImageUrl = new List<string>(imageUrls);
+        }
+
+        public HomeworkViewModel(string studentName, IEnumerable<string> imageUrls)
+        {
+            StudentName = studentName;
+
+            ImageUrl = new List<string>(imageUrls);
+        }
+    }
+}
